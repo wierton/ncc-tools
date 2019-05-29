1e074cc7d463412889c9259609ee0c73cd448a92
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2019-05-29 11:21:27 +0800

    test memory limit and time limit

--- main.cc
+++ main.cc
@@ -21,6 +21,8 @@ int main(int argc, const char *argv[]) {
   using namespace irsim;
   Compiler compiler;
   auto prog = compiler.compile(ifs);
+  prog->setInstsLimit(1000);
+  prog->setMemoryLimit(1000);
   auto code = prog->run(compiler.getFunction("main"));
   printf("ret with %d, reason %d\n", code, (int)prog->exception);
   return 0;
